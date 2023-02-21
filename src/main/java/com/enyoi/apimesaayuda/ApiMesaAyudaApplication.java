eb23b7348b0eadb9b437015fa499dfbe38369c8c
D David Galeano P <114103145+DDavidGalP@users.noreply.github.com>
2023-02-21 01:01:15 -0500

    server o

--- src/main/java/com/enyoi/apimesaayuda/ApiMesaAyudaApplication.java
+++ src/main/java/com/enyoi/apimesaayuda/ApiMesaAyudaApplication.java
@@ -2,6 +2,9 @@ package com.enyoi.apimesaayuda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApiMesaAyudaApplication {
@@ -9,6 +12,16 @@ public class ApiMesaAyudaApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(ApiMesaAyudaApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api-mesa-ayuda/**").allowedOrigins("http://api-mesa-ayuda-production.up.railway.app").allowedMethods("*").allowedHeaders("*");
+			}
+		};
 	}
 
 }
--- src/main/java/com/enyoi/apimesaayuda/base/configs/ApplicationConfig.java
+++ src/main/java/com/enyoi/apimesaayuda/base/configs/ApplicationConfig.java
@@ -6,17 +6,11 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Arrays;
-import java.util.List;
-
 @Configuration
-public class ApplicationConfig implements WebMvcConfigurer {
+public class ApplicationConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
@@ -30,15 +24,7 @@ public class ApplicationConfig implements WebMvcConfigurer {
                                 new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
     }
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedMethods("*")
-                .allowedOrigins("http://api-mesa-ayuda-production.up.railway.app")
-                .allowedHeaders("*")
-                .allowCredentials(false)
-                .maxAge(-1);
-    }
+
 
 
 
