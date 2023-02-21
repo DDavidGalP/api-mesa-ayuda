b442e2c6f7bf7734371fb76f79d9f14cb4a2a45d
D David Galeano P <114103145+DDavidGalP@users.noreply.github.com>
2023-02-21 00:07:29 -0500

    server origin

--- src/main/java/com/enyoi/apimesaayuda/base/configs/ApplicationConfig.java
+++ src/main/java/com/enyoi/apimesaayuda/base/configs/ApplicationConfig.java
@@ -6,8 +6,14 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class ApplicationConfig {
@@ -24,19 +30,16 @@ public class ApplicationConfig {
                                 new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
     }
 
-   @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api-mesa-ayuda/**")
-                 .allowedOrigins("https://api-mesa-ayuda-production.up.railway.app")
-                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                 .maxAge(3600);
-                 }
-
-                 };
-                 }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://api-mesa-ayuda-production.up.railway.app"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT"));
+        configuration.setAllowedHeaders(List.of("Authorization"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
 
 
